db87f5b7f3b23d49efea6f9a7b7e40159be492bf
Mohammed Kebdani Ghazi <mohaborges@MacBook-de-Moha.local>
2024-05-04 23:43:42 +0200

    Añadidos el PUT de challenge

--- src/main/java/com/ctflags/controller/ChallengeController.java
+++ src/main/java/com/ctflags/controller/ChallengeController.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.ctflags.entities.Challenge;
-import com.ctflags.entities.User;
 import com.ctflags.services.ChallengeService;
 
 @RestController
@@ -36,6 +39,38 @@ public class ChallengeController {
 	public Challenge saveChallenge(@RequestBody Challenge challenge) {
 		System.out.println(challenge);
 		return challengeService.saveChallenge(challenge);
+	}
+	
+	@PutMapping("/challenge/{id}")
+    public ResponseEntity<Challenge> updateChallenge(@PathVariable Long id, @RequestBody Challenge updatedChallenge) {
+        Optional<Challenge> existingChallengeOptional = challengeService.findChallengeById(id);
+        if (existingChallengeOptional.isPresent()) {
+            Challenge existingChallenge = existingChallengeOptional.get();
+            // Actualiza todos los campos del desafío existente con los datos proporcionados
+            existingChallenge.setName(updatedChallenge.getName());
+            existingChallenge.setDescription(updatedChallenge.getDescription());
+            existingChallenge.setFlag(updatedChallenge.getFlag());
+            existingChallenge.setPoints(updatedChallenge.getPoints());
+            existingChallenge.setDifficulty(updatedChallenge.getDifficulty());
+            // Actualiza otros campos según sea necesario
+            
+            // Guarda el desafío actualizado
+            Challenge savedChallenge = challengeService.saveChallenge(existingChallenge);
+            
+            return ResponseEntity.ok(savedChallenge);
+        } else {
+            // Si no se encuentra el desafío con el ID proporcionado, retorna un código de estado 404
+            throw new ResourceNotFoundException("Challenge not found with id: " + id);
+        }
+    }
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public class ResourceNotFoundException extends RuntimeException {
+	    private static final long serialVersionUID = 1L;
+
+		public ResourceNotFoundException(String message) {
+	        super(message);
+	    }
 	}
 	
 	@DeleteMapping("/challenge/{id}")
---
+++ src/main/java/com/ctflags/controller/HttpStatus.java
@@ -0,0 +1,7 @@
+package com.ctflags.controller;
+
+public class HttpStatus {
+
+	public static final org.springframework.http.HttpStatus NOT_FOUND = null;
+
+}
